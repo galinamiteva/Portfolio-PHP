618b6b45f4c7c1aa2d247f04901cf9f5afde1d7e
galinamiteva <70331948+galinamiteva@users.noreply.github.com>
2021-08-09 02:28:30 +0200

    Update Search.js

--- portfolio/src/modules/Search.js
+++ portfolio/src/modules/Search.js
@@ -57,19 +57,19 @@ class Search {
       this.resultsDiv.innerHTML = `
         <div class="row">
           <div class="one-third">
-            <h2 class="search-overlay__section-title">General Information</h2>
-            ${results.generalInfo.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
-              ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a> ${item.postType == "post" ? `by ${item.authorName}` : ""}</li>`).join("")}
+            <h2 class="search-overlay__section-title">Allmän information</h2>
+            ${results.generalInfo.length ? '<ul class="link-list min-list">' : "<p>Ingen allmän information matchar den sökningen.</p>"}
+              ${results.generalInfo.map(item => `<li><a href="${item.permalink}">${item.title}</a> ${item.postType == "post" ? `av ${item.authorName}` : ""}</li>`).join("")}
             ${results.generalInfo.length ? "</ul>" : ""}
           </div>
           <div class="one-third">
-            <h2 class="search-overlay__section-title">Programs</h2>
-            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs match that search. <a href="${universityData.root_url}/programs">View all programs</a></p>`}
+            <h2 class="search-overlay__section-title">Program</h2>
+            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>Inga program matchar sökningen. <a href="${universityData.root_url}/programs">Visa alla program</a></p>`}
               ${results.programs.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
             ${results.programs.length ? "</ul>" : ""}
 
-            <h2 class="search-overlay__section-title">Professors</h2>
-            ${results.professors.length ? '<ul class="professor-cards">' : `<p>No professors match that search.</p>`}
+            <h2 class="search-overlay__section-title">Professorer</h2>
+            ${results.professors.length ? '<ul class="professor-cards">' : `<p>Inga professorer matchar den sökningen.</p>`}
               ${results.professors
                 .map(
                   item => `
@@ -86,13 +86,13 @@ class Search {
 
           </div>
           <div class="one-third">
-            <h2 class="search-overlay__section-title">Campuses</h2>
-            ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No campuses match that search. <a href="${universityData.root_url}/campuses">View all campuses</a></p>`}
+            <h2 class="search-overlay__section-title">Campus</h2>
+            ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>Inga campus matchar sökningen. <a href="${universityData.root_url}/campuses">Visa alla campus</a></p>`}
               ${results.campuses.map(item => `<li><a href="${item.permalink}">${item.title}</a></li>`).join("")}
             ${results.campuses.length ? "</ul>" : ""}
 
-            <h2 class="search-overlay__section-title">Events</h2>
-            ${results.events.length ? "" : `<p>No events match that search. <a href="${universityData.root_url}/events">View all events</a></p>`}
+            <h2 class="search-overlay__section-title">Händelser</h2>
+            ${results.events.length ? "" : `<p>Inga händelser matchar sökningen. <a href="${universityData.root_url}/events">Visa alla händelser.</a></p>`}
               ${results.events
                 .map(
                   item => `
@@ -103,7 +103,7 @@ class Search {
                   </a>
                   <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
-                    <p>${item.description} <a href="${item.permalink}" class="nu gray">Learn more</a></p>
+                    <p>${item.description} <a href="${item.permalink}" class="nu gray">Läs mer</a></p>
                   </div>
                 </div>
               `
@@ -154,7 +154,7 @@ class Search {
         <div class="search-overlay__top">
           <div class="container">
             <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <input type="text" class="search-term" placeholder="Vad letar du efter?" id="search-term">
             <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
           </div>
         </div>
